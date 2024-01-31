4b7621b7ded6d1ce57419a20ceeebab0f12002c4
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-01-31 09:45:25 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
+++ 240131/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.py
@@ -1,13 +1,14 @@
 n = int(input())
 
-
 def go(line, star):
-    print('*' * star)
-    if line == n * 2 - 1:
+    print('* ' * star)
+    if line == n * 2:
         return
 
     if line < n:
         go(line + 1, star - 1)
+    elif line == n:
+        go(line + 1, star)
     else:
         go(line + 1, star + 1)
 
