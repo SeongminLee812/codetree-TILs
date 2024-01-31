208b6c85ced39ffef0c612f548aaf2319b4c636b
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-01-31 20:13:39 +0900

    Title: 되돌아오기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240131/되돌아오기/come-back.py
+++ 240131/되돌아오기/come-back.py
@@ -8,18 +8,18 @@ dys = [1, 0, -1, 0]
 
 dir_num = {'E': 0, 'S': 1, 'W': 2, 'N': 3}
 
-ok = False
-for _ in range(n):
-    direct, num = input().split()
-    num = int(num)
-    for _ in range(num):
-        x += dxs[dir_num[direct]]
-        y += dys[dir_num[direct]]
-        t += 1
-        if x == 0 and y == 0:
-            print(t)
-            ok = True
-            break
-
-if not ok:
+def go():
+    global x, y, t
+    for _ in range(n):
+        direct, num = input().split()
+        num = int(num)
+        for _ in range(num):
+            x += dxs[dir_num[direct]]
+            y += dys[dir_num[direct]]
+            t += 1
+            if x == 0 and y == 0:
+                print(t)
+                not_found = False
+                return
     print(-1)
+go()
