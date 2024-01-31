d0244f0e4bbdcd8dc29c699f77b24a14ffeecd3c
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-01-31 10:32:20 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 123ms, Memory: 26MB, Status: Passed - CodeTree

--- 240131/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
+++ 240131/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.py
@@ -1,25 +1,25 @@
 n = int(input())
-a = list(map(int, input().split()))
+arr = [0] + list(map(int, input().split()))
 
-limit = 1
-for i in a:
-    limit *= i
+# 두 수의 최대공약수
+def gcd(x, y):
+    if y == 0:
+        return x
+    return gcd(y, x % y)
 
-def go(num):
-    if num == limit:
-        print(limit)
-        return limit
+# 두 수의 최소공배수
+def lcm(x, y):
+    return (x * y) // gcd(x, y)
 
-    baesu = True
-    for i in a:
-        if num % i != 0:
-            baesu = False
+# index번째까지 인덱스의 숫자중에 가장 큰 값을 반환
+def get_lcm_all(index):
+    # 남은 원소가 1개라면 자기 자신이 답
+    if index == 1:
+        return arr[1]
 
-    if baesu:
-        print(num)
-        return num
-    else:
-        go(num + MAX)
+    # 1번째 ~ indedx - 1번째 원소의 최소공배수를 구한 결과와
+    # 현재 index 원소의 최소공배수를 구하여 반환
+    return lcm(get_lcm_all(index - 1), arr[index])
 
-MAX = max(a)
-go(max(a))
+# 모든 수의 최소공배수를 구함
+print(get_lcm_all(n))
