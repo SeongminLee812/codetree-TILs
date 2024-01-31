2fb2b96e8f27ba0b427bc78ee6966173f11ced35
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-01-31 11:02:32 +0900

    Title: 비오는 날, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240131/비오는 날/rainy-day.py
+++ 240131/비오는 날/rainy-day.py
@@ -1,16 +1,42 @@
 n = int(input())
 
-days = []
+faster = '2101-01-01'
+wkdy = ''
+whe = ''
 
+＃ 시간을 고려하여 작성
 for _ in range(n):
-    day, weekday, whether = input().split()
+    days, weekday, whether = input().split()
     if whether == 'Rain':
-        days.append(day + ' ' + weekday + ' ' + whether)
+        y, m, d = days.split('-')
+        prev_y, prev_m, prev_d = faster.split('-')
+        if y < prev_y:
+            faster, wkdy, whe = days, weekday, whether
+        elif y == prev_y:
+            if m < prev_m:
+                faster, wkdy, whe = days, weekday, whether
+            elif m == prev_m:
+                if d < prev_d:
+                    faster, wkdy, whe = days, weekday, whether
 
-def sorting(text: str) -> str:
-    days, weekday, whether = text.split()
-    y, m, d = days.split('-')
-    return y, m, d
+print(" ".join(map(str, [faster, wkdy, whe])))
 
-days.sort(key=sorting)
-print(days[0])
+
+＃ 시간을 고려하지 않고 편한 코드
+
+# n = int(input())
+
+# days = []
+
+# for _ in range(n):
+#     day, weekday, whether = input().split()
+#     if whether == 'Rain':
+#         days.append(day + ' ' + weekday + ' ' + whether)
+
+# def sorting(text: str) -> str:
+#     days, weekday, whether = text.split()
+#     y, m, d = days.split('-')
+#     return y, m, d
+
+# days.sort(key=sorting)
+# print(days[0])
