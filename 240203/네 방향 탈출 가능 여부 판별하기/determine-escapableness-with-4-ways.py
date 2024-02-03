a40d61fb06d0cd2d10494ca348a28fe6d4de0911
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-03 10:31:24 +0900

    Title: 네 방향 탈출 가능 여부 판별하기, Time: 246ms, Memory: 27MB, Status: Passed - CodeTree

--- 240203/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
+++ 240203/네 방향 탈출 가능 여부 판별하기/determine-escapableness-with-4-ways.py
@@ -5,7 +5,7 @@ a = [list(map(int, input().split())) for _ in range(n)]
 visited = [[False] * m for _ in range(n)]
 
 def in_range(x, y):
-    return x >= 0 and x < n and y >= 0 and y < n
+    return x >= 0 and x < n and y >= 0 and y < m
 
 def can_go(x, y):
     if not in_range(x, y):
