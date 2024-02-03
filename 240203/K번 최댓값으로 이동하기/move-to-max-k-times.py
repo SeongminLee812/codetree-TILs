107f251e3945190198d30b6ddd2302b068810ae1
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-03 13:24:31 +0900

    Title: K번 최댓값으로 이동하기, Time: 270ms, Memory: 28MB, Status: Passed - CodeTree

--- 240203/K번 최댓값으로 이동하기/move-to-max-k-times.py
+++ 240203/K번 최댓값으로 이동하기/move-to-max-k-times.py
@@ -3,7 +3,6 @@ from collections import deque
 # get input
 n, k = map(int, input().split())
 a = [list(map(int, input().split())) for _ in range(n)]
-
 r, c = map(int, input().split())
 
 def in_range(x, y):
@@ -18,11 +17,6 @@ def can_go(x, y, value):
         return False
     return True
 
-def push(x, y):
-    global q, visited
-    visited[x][y] = True
-    q.append((x, y))
-
 def bfs(value):
     global q, visited
     global max_val, max_pos
@@ -34,32 +28,37 @@ def bfs(value):
         x, y = q.popleft()
 
         for dx, dy in zip(dxs, dys):
-            nx, ny = x + dx, y + dy
+            nx = x + dx
+            ny = y + dy
             if can_go(nx, ny, value):
-                push(nx, ny)
-                max_val = max(max_val, a[nx][ny]) #처음 탐색한 곳은 max_val이 0이므로 들어가 버림
+                visited[nx][ny] = True
+                q.append((nx, ny))
+                max_val = max(max_val, a[nx][ny]) # max_val 초기값이 0이므로 첫 탐색시 무조건 max pos에 입력되는 것을 방지
 
     for i in range(n):
         for j in range(n):
-            if a[i][j] == max_val:
+            if visited[i][j] and a[i][j] == max_val:
                 max_pos.append((i, j))
 
-q = deque()
-q.append((r - 1, c - 1))
+
 ans = (r - 1, c - 1)
+x, y = r - 1, c - 1
 
 for _ in range(k):
+    q = deque()
+
     max_val = 0
     max_pos = []
     visited = [[False] * n for _ in range(n)]
-    x, y = q[0]
+
     visited[x][y] = True
+    q.append((x, y))
     value = a[x][y]
     bfs(value)
     if not max_pos:
         break
-    max_pos.sort(key=lambda temp: (temp[0], temp[1]))
-    push(*max_pos[0])
+    max_pos.sort(key=lambda temp: (temp[0], temp[1])) # 정렬하여 우선순위 도출
+    x, y = max_pos[0]
     ans = max_pos[0]
 
 print(ans[0] + 1, ans[1] + 1)
