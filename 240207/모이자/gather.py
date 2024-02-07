199be5dbf191d5ecb9e9d4a85f9928d2d2090c7e
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-07 12:16:47 +0900

    Title: 모이자, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

--- 240207/모이자/gather.py
+++ 240207/모이자/gather.py
@@ -1,14 +1,14 @@
 import sys
 
 n = int(input())
-a = list(map(int, input().split()))
+a = [0] + list(map(int, input().split()))
 
 min_val = sys.maxsize
 
-for i in range(n):
+for i in range(1, n + 1):
     sum_val = 0
-    for j in range(n):
-        sum_val += abs(a[i] - a[j]) * j
+    for j in range(1, n + 1):
+        sum_val += abs(i - j) * a[j]
     min_val = min(min_val, sum_val)
 
 print(min_val)
