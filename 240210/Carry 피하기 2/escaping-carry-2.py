21b553a4e7fac7b4d75479c9659b221dc20d6bff
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-10 10:55:17 +0900

    Title: Carry 피하기 2, Time: 315ms, Memory: 28MB, Status: Passed - CodeTree

--- 240210/Carry 피하기 2/escaping-carry-2.py
+++ 240210/Carry 피하기 2/escaping-carry-2.py
@@ -28,6 +28,6 @@ for i in range(n):
     for j in range(i + 1, n):
         for k in range(j + 1, n):
             if check([arr[i], arr[j], arr[k]]):
-                ans = arr[i] + arr[j] + arr[k]
+                ans = max(ans, arr[i] + arr[j] + arr[k])
 
 print(ans)
