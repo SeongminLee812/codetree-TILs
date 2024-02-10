6dfeede2fb4b2c7e0a0cbab02efe609f11124f99
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-10 13:29:14 +0900

    Title: G or H 3, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240210/G or H 3/G-or-H-3.py
@@ -0,0 +1,25 @@
+n, k = map(int, input().split())
+
+placed = [0] * 10001
+length = 0
+
+for _ in range(n):
+    a, b = input().split()
+    a = int(a)
+    if b == 'G':
+        placed[a] = 1
+    elif b == 'H':
+        placed[a] = 2
+    length = max(length, a)
+
+placed = placed[:length + 1]
+
+ans = 0
+for i in range(length - k + 1):
+    val = 0
+    for j in range(i, i + k + 1):
+        val += placed[j]
+    print()
+    ans = max(ans, val)
+
+print(ans)
