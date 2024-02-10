e021bb93f222a2804db9d9099fed9af4223583fc
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-10 14:12:10 +0900

    Title: 아름다운 수열 2, Time: 333ms, Memory: 25MB, Status: Passed - CodeTree

--- 240210/아름다운 수열 2/beautiful-sequence-2.py
+++ 240210/아름다운 수열 2/beautiful-sequence-2.py
@@ -6,28 +6,12 @@ n, m = map(int, input().split())
 arr_a = list(map(int, input().split()))
 arr_b = list(map(int, input().split()))
 
-beautiful = defaultdict(int)
-for i in range(m):
-    beautiful[arr_b[i]] += 1
-
-def check(num_array, check_dict):
-    check_dict = copy.deepcopy(check_dict)
-    for i in range(len(num_array)):
-        check_dict[num_array[i]] -= 1
-        if check_dict[num_array[i]] < 0:
-            return False
-
-    for value in check_dict.values():
-        if value != 0:
-            return False
-    return True
+check_array = sorted(arr_b)
 
 
 ans = 0
-for i in range(n):
-    for j in range(i, n):
-        elems = arr_a[i:j + 1]
-        if check(elems, beautiful):
-            ans += 1
+for i in range(n - m + 1):
+    if sorted(arr_a[i: i + m]) == check_array:
+        ans += 1
 
 print(ans)
