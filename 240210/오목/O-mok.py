160898d1eb08c6da0292d2610a619dde8e23a2bf
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-10 11:38:29 +0900

    Title: 오목, Time: 344ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 240210/오목/O-mok.py
+++ 240210/오목/O-mok.py
@@ -7,6 +7,7 @@ def is_win(x, y):
     direction1 = [0, 0, 0, 0], [1, 2, 3, 4] # 오른쪽 가로 방향
     direction2 = [1, 2, 3, 4], [0, 0, 0, 0] # to downward
     direction3 = [1, 2, 3, 4], [1, 2, 3, 4] # to downward and rightward
+    direction4 = [1, 2, 3, 4], [-1, -2, -3, -4] # to downward and left
 
     dr1_ok = True
     for dx, dy in zip(direction1[0], direction1[1]):
@@ -29,6 +30,13 @@ def is_win(x, y):
             dr3_ok = False
             break
 
+    dr4_ok = True
+    for dx, dy in zip(direction4[0], direction4[1]):
+        nx, ny = x + dx, y + dy
+        if not in_range(nx, ny) or a[x][y] != a[nx][ny]:
+            dr4_ok = False
+            break
+
     # print(f'dr1_ok={dr1_ok}, dr2_ok={dr2_ok}, dr3_ok={dr3_ok}')
 
     if dr1_ok:
@@ -37,6 +45,8 @@ def is_win(x, y):
         return x + 2 + 1, y + 1
     if dr3_ok:
         return x + 2 + 1, y + 2 + 1
+    if dr4_ok:
+        return x + 2 + 1, y - 2 + 1
     return False
 
 a = [list(map(int, input().split())) for _ in range(19)]
