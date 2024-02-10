e9d0ffbf093d15a531aa279b8b91aaf515f80e46
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-10 18:57:02 +0900

    Title: 숫자 카운트, Time: 124ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240210/숫자 카운트/numeric-count.py
@@ -0,0 +1,33 @@
+n = int(input())
+arr = []
+for _ in range(n):
+    num, strike, ball = map(int, input().split())
+    arr.append((num, strike, ball))
+
+def check(my_num: str, target: str, t_strike: int, t_ball: int):
+    strike = 0
+    ball = 0
+    for i in range(3):
+        if my_num[i] == target[i]:
+            strike += 1
+        elif my_num[i] == target[i - 1] or my_num[i] == target[i - 2]:
+            ball += 1
+    if strike == t_strike and ball == t_ball:
+        return True
+    else:
+        return False
+
+
+ans = 0
+for i in range(1, 10):
+    for j in range(1, 10):
+        for k in range(1, 10):
+            ok = True
+            my_num = str(i) + str(j) + str(k)
+            for target, strike, ball in arr:
+                if not check(my_num, str(target), strike, ball):
+                    ok = False
+            if ok:
+                # print(my_num)
+                ans += 1
+print(ans)
