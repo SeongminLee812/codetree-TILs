b0dec703384d50fb9fe96f6b1bc3385d01e242ef
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-11 10:41:20 +0900

    Title: 겹치지 않는 선분 2, Time: 175ms, Memory: 25MB, Status: Passed - CodeTree

--- 240211/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 240211/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -6,7 +6,7 @@ for _ in range(n):
     lines.append((x1, x2))
 
 def check(first, second):
-    if first[0] <= second[0] and second[1] <= second[0]:
+    if first[0] <= second[0] and second[1] <= first[1]:
         return False
     if second[0] <= first[0] and first[1] <= second[1]:
         return False
