f76039c859b35b7fc57e925b28a8534e21b65510
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-11 16:27:22 +0900

    Title: 등차수열, Time: 168ms, Memory: 25MB, Status: Passed - CodeTree

--- 240211/등차수열/arithmetic-sequence.py
+++ 240211/등차수열/arithmetic-sequence.py
@@ -3,11 +3,14 @@ n = int(input())
 arr = list(map(int, input().split()))
 arr.sort()
 
+
 ans = 0
 for k in range(min(arr) + 1, max(arr)):
+    cnt = 0
     for i in range(n - 1):
         for j in range(i + 1, n):
             if k - arr[i] == arr[j] - k:
-                ans += 1
+                cnt += 1
+    ans = max(ans, cnt)
 
 print(ans)
