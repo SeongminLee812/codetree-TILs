07e95cc3eca1d643f670cbe69c082671764a5182
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-11 10:35:58 +0900

    Title: 겹치지 않는 선분 2, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240211/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -0,0 +1,23 @@
+n = int(input())
+lines = []
+for _ in range(n):
+    x1, x2 = map(int, input().split())
+    # x1, x2 = x1 + 1000000, x2 + 1000000
+    lines.append((x1, x2))
+
+def check(first, second):
+    if first[0] <= second[0] and second[1] <= second[0]:
+        return False
+    if second[0] <= first[0] and first[1] <= second[1]:
+        return False
+    return True
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if i == j:
+            continue
+        if check(lines[i], lines[j]):
+            ans += 1
+
+print(ans)
