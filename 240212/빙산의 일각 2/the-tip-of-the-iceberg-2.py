1055d7f67ccc0a20da3a31ff450ed1b506ec3a50
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-12 11:40:08 +0900

    Title: 빙산의 일각 2, Time: 166ms, Memory: 25MB, Status: Passed - CodeTree

--- 240212/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240212/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -1,31 +1,15 @@
-import copy
-
 n = int(input())
 h = [int(input()) for _ in range(n)]
 
-def count_ice(array):
-    count = 0
-    for i in range(len(array) - 1):
-        if (array[i] > 0 and array[i + 1] == 0) or (i + 1 == len(array) - 1 and array[i + 1] > 0):
-            count += 1
-    return count
-
 ans = 0
-
-final_h = []
-final_cnt = 0
-
 for i in range(0, max(h) + 1):
-    height = i
-    now_h = copy.deepcopy(h)
-    for j in range(n):
-        now_h[j] -= height
-        if now_h[j] < 0:
-            now_h[j] = 0
-    cnt = count_ice(now_h)
+    cnt = 0
+    if h[0] > i:
+        cnt += 1
 
-    if cnt > ans:
-        final_h = now_h
-        ans = cnt
+    for j in range(1, n):
+        if h[j - 1] <= i and h[j] > i:
+            cnt += 1
+    ans = max(ans, cnt)
 
 print(ans)
