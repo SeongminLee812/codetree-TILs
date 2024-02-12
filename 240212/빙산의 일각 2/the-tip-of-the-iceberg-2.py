4a94d8a110d7df083a756f2ef69a8e2015f67578
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-12 11:25:01 +0900

    Title: 빙산의 일각 2, Time: 213ms, Memory: 26MB, Status: Passed - CodeTree

--- 240212/빙산의 일각 2/the-tip-of-the-iceberg-2.py
+++ 240212/빙산의 일각 2/the-tip-of-the-iceberg-2.py
@@ -5,21 +5,15 @@ h = [int(input()) for _ in range(n)]
 
 def count_ice(array):
     count = 0
-    i = 0
-    j = len(array) - 1
-    if len(array) % 2 != 0: # 홀수인경우 패딩을 붙임
-        array += [0]
-        j += 1
-    while i < j:
-        if array[i] > 0 and array[i + 1] == 0:
+    for i in range(len(array) - 1):
+        if (array[i] > 0 and array[i + 1] == 0) or (i + 1 == len(array) - 1 and array[i + 1] > 0):
             count += 1
-        if array[j] > 0 and array[j - 1] == 0:
-            count += 1
-        i += 1
-        j -= 1
     return count
 
 ans = 0
+
+final_h = []
+final_cnt = 0
 
 for i in range(0, max(h) + 1):
     height = i
@@ -29,7 +23,9 @@ for i in range(0, max(h) + 1):
         if now_h[j] < 0:
             now_h[j] = 0
     cnt = count_ice(now_h)
-    # print(now_h, f'\tcount={cnt}')
-    ans = max(ans, cnt)
+
+    if cnt > ans:
+        final_h = now_h
+        ans = cnt
 
 print(ans)
