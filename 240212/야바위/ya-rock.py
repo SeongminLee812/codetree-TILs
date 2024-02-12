2fd3eec742713e8d7d6b8666371dceb7ea71e8eb
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-12 11:59:15 +0900

    Title: 야바위, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240212/야바위/ya-rock.py
+++ 240212/야바위/ya-rock.py
@@ -1,7 +1,7 @@
 n = int(input())
 directions_list = [list(map(int, input().split())) for _ in range(n)]
 
-placed = [0, 0, 0, 0]
+placed = [0, 0, 0]
 
 def swap(a, b):
     placed[a], placed[b] = placed[b], placed[a]
@@ -11,10 +11,13 @@ for i in range(3):
     placed[i] = 1
     score = 0
     for a, b, c in directions_list:
+        a -= 1
+        b -= 1
+        c -= 1
         swap(a, b)
         if placed[c] == 1:
             score += 1
     ans = max(ans, score)
-    placed[i] = 0
+    placed = [0, 0, 0]
 
 print(ans)
