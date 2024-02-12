63c17422b43552622e70b98b796a1d502a20ae31
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-12 15:47:52 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 163ms, Memory: 72MB, Status: Passed - CodeTree

--- 240212/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
+++ 240212/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.py
@@ -2,13 +2,13 @@ n = int(input())
 string = input()
 
 max_len = 0
-for window_size in range(1, n // 2 + 1):
+for window_size in range(1, n):
     ok = False
     for index in range(n):
         query = string[index:index + window_size]
         # print(f'window_size={window_size}, index={index}, query={query}')
 
-        for search_index in range(index + window_size, n - window_size + 1):
+        for search_index in range(index + 1, n - window_size + 1):
             if string[search_index:search_index + window_size] == query:
                 ok = True
                 # print('!!!!!!!!!!!!!!!!!check!!!!!!!!!!!!!!!!!!!!!')
