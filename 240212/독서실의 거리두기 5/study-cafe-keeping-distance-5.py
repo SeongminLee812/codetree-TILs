0643a3bd08fedc704192ebc05dc28bfd68079074
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-12 16:49:09 +0900

    Title: 독서실의 거리두기 5, Time: 160ms, Memory: 73MB, Status: Passed - CodeTree

--- 240212/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 240212/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -19,26 +19,15 @@ def cal_dist(array):
             min_diff = min(diff, min_diff)
     return min_diff
 
-ans = 0
+ans = 1
 
 for i in range(n):
     # 새로운 한명 투입 가능 여부 확인
     dist = 0
-    if i == n - 1:
-        if arr[i] == 0 and arr[i - 1] == 0:
-            arr[i] = 1
-            dist = cal_dist(arr)
-            arr[i] = 0
-    elif i == 0:
-        if arr[i] == 0 and arr[i + 1] == 0:
-            arr[i] = 1
-            dist = cal_dist(arr)
-            arr[i] = 0
-    else:
-        if arr[i] == 0 and arr[i - 1] == 0 and arr[i + 1] == 0:
-            arr[i] = 1
-            dist = cal_dist(arr)
-            arr[i] = 0
+    if arr[i] == 0:
+        arr[i] = 1
+        dist = cal_dist(arr)
+        arr[i] = 0
 
     ans = max(ans, dist)
 
