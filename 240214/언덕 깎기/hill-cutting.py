b40bae30853f7d7b817a82cf6d1dda4b7bdb39bd
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-14 12:21:29 +0900

    Title: 언덕 깎기, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240214/언덕 깎기/hill-cutting.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(input())
+mount = [int(input()) for _ in range(n)]
+
+def check_cost(min_height):
+    cost = 0
+    for elem in mount:
+        if min_height <= elem <= min_height + 17:
+            continue
+        else:
+            if elem > min_height + 17:
+                cost += (elem - (min_height + 17)) ** 2
+            elif elem < min_height:
+                cost += (min_height - elem) ** 2
+    return cost
+
+min_cost = sys.maxsize
+for i in range(0, max(mount) +1):
+    min_cost = min(min_cost, check_cost(i))
+
+print(min_cost)
