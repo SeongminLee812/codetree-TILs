6330e9f23249c471c9df0bf59888e545708f8ddb
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-14 11:08:52 +0900

    Title: 독서실의 거리두기 4, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240214/독서실의 거리두기 4/study-cafe-keeping-distance-4.py
@@ -0,0 +1,31 @@
+import sys
+
+n = int(input())
+arr = list(map(int, input()))
+
+def minimun_distance():
+    """가장 가까운 두 사람"""
+    min_dist = sys.maxsize
+    prev_i = 0
+    for i in range(1, n):
+        if arr[i] == 1:
+            dist = i - prev_i
+            min_dist = min(min_dist, dist)
+            prev_i = i
+    return min_dist
+
+
+max_dist = 0
+for i in range(n - 1):
+    if arr[i] == 0:
+        arr[i] = 1
+        for j in range(i + 1, n):
+            if arr[j] == 0:
+                arr[j] = 1
+                dist = minimun_distance()
+                max_dist = max(max_dist, dist)
+                arr[j] = 0
+
+        arr[i] = 0
+
+print(max_dist)
