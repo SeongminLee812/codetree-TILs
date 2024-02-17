a1963955bba3b22fb2fd17a621f1dad237a63f2e
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-17 12:30:36 +0900

    Title: 구간 잘 나누기, Time: 170ms, Memory: 25MB, Status: Passed - CodeTree

--- 240217/구간 잘 나누기/divide-sections-well.py
+++ 240217/구간 잘 나누기/divide-sections-well.py
@@ -1,17 +1,25 @@
+import sys
+
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-# 합으로 가능한 최소값을 limit으로 지정
-for limit in range(1, 100 * 100 + 1):
-    # arr의 앞에서부터 탐색
-    prev_i = 0
-    partition = 0
-    for i in range(n):
-        if limit < sum(arr[prev_i: i + 1]):
-            partition += 1
-            prev_i = i
+ans = sys.maxsize
 
-    if partition == m - 1:
-        limit = max(limit, max(arr))
-        print(limit)
-        break
+# 합으로 가능한 최소값을 limit으로 지정
+for limit in range(max(arr), 100 * 100 + 1):
+    now_val = 0
+    partition = 0
+    max_val = 0
+    # arr의 앞에서부터 탐색
+    for i in range(n):
+        if now_val + arr[i] > limit:
+            now_val = 0
+            partition += 1
+
+        now_val += arr[i]
+        max_val = max(now_val, max_val)
+
+    if partition <= m - 1:
+        ans = min(ans, max_val)
+
+print(ans)
