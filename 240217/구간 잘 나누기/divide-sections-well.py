0fe56e5482cfdd011e935f4d3a0d6bfbefd4d4a3
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-17 10:15:31 +0900

    Title: 구간 잘 나누기, Time: 117ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240217/구간 잘 나누기/divide-sections-well.py
+++ 240217/구간 잘 나누기/divide-sections-well.py
@@ -1,1 +1,16 @@
-print(9)
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+# 합으로 가능한 최소값을 limit으로 지정
+for limit in range(1, 100 * 100 + 1):
+    # arr의 앞에서부터 탐색
+    prev_i = 0
+    partition = 0
+    for i in range(n):
+        if limit < sum(arr[prev_i: i + 1]):
+            partition += 1
+            prev_i = i
+
+    if partition == m - 1:
+        print(limit)
+        break
