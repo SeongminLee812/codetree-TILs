8141f0c75c8a283465e03b2fe4049658cba0db25
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-18 16:32:47 +0900

    Title: Date to Date, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

--- 240218/Date to Date/date-to-date.py
+++ 240218/Date to Date/date-to-date.py
@@ -1,19 +1,8 @@
 m1, d1, m2, d2 = map(int, input().split())
 
-#                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
-num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days_of_month = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-month, day = m1, d1
-elapsed_days = 1
+later = sum([days_of_month[i] for i in range(1, m2)]) + d2 + 1
+faster = sum([days_of_month[i] for i in range(1, m1)]) + d1
 
-while True:
-    if month == m2 and day == d2:
-        break
-
-    day += 1
-    elapsed_days += 1
-    if day > num_of_days[month]:
-        month += 1
-        day = 1
-
-print(elapsed_days)
+print(later - faster)
