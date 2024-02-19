9ded066371da4a40ac3bfbb372394d3f1ac8caee
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-19 21:34:13 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240219/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.py
@@ -0,0 +1,23 @@
+def check(A_short, B):
+    if len(A_short) < len(B):
+        return False
+    for i in range(len(B)):
+        if A_short[i] != B[i]:
+            return False
+    return True
+
+def is_continuous(A, B):
+    for j in range(len(A)):
+        if A[j] == B[0]:
+            if check(A[j:], B):
+                return True
+    return False
+
+n1, n2 = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+if is_continuous(A, B):
+    print('Yes')
+else:
+    print('No')
