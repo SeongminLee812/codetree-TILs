ed152e8258b7b9fc8400cc00d30e481bf17f5082
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-19 20:34:34 +0900

    Title: 함수를 이용한 소수 판별, Time: 145ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240219/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,21 @@
+primes = [True] * 101
+for i in range(2, 101 // 2):
+    num = i + i
+    while num <= 100:
+        primes[num] = False
+        num += i
+
+def is_prime(num):
+    return primes[num]
+
+def how_many_prime(a, b):
+    total = 0
+    for i in range(a, b + 1):
+        if is_prime(i):
+            total += i
+
+    return total
+
+a, b = map(int, input().split())
+
+print(how_many_prime(a, b))
