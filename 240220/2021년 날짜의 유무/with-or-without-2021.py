7474b25a3b4fdac0c5bb580e60a49bdbdade1047
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-20 20:01:21 +0900

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240220/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,14 @@
+m, d = map(int, input().split())
+
+def is_possible(month, day):
+    days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    if day <= days[month]:
+        return True
+    else:
+        return False
+
+if is_possible(m, d):
+    print('Yes')
+else:
+    print('No')
