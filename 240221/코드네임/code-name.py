78b3f665ececbe1e1e96f6637a567404c3e2a740
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-21 20:08:26 +0900

    Title: 코드네임, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240221/코드네임/code-name.py
@@ -0,0 +1,14 @@
+class Spy:
+    def __init__(self, code_name, score):
+        self.code_name = code_name
+        self.score = score
+
+    def __repr__(self):
+        return f'{self.code_name} {self.score}'
+
+spies = [
+    Spy(*tuple(input().split()))
+    for _ in range(5)
+]
+
+print(sorted(spies, key=lambda x: int(x.score))[0])
