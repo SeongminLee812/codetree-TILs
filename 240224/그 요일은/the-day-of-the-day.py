c3c7e2de503d009e69a1db292660c3f70769585e
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-24 10:35:07 +0900

    Title: 그 요일은, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240224/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,24 @@
+days_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+m1, d1, m2, d2 = map(int, input().split())
+count_weekday = input()
+
+# 요일 차이만큼 빼주기 위해서 요일 차이 미리 계산 
+minus = 0
+for i in range(len(weekdays)):
+    if weekdays[i] == count_weekday:
+        minus = i
+
+end_day = sum([days_of_month[m] for m in range(m2)]) + d2
+start_day = sum([days_of_month[m] for m in range(m1)]) + d1 + minus
+
+elapsed_day = end_day - start_day
+
+ans = 0
+if elapsed_day >= minus:
+    ans = elapsed_day // 7 + 1
+else:
+    ans = elapsed_day // 7
+
+print(ans)
