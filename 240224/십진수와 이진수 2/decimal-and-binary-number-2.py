96ad5ee61f9a1f05411495ed2420b41ff71fca78
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-24 11:14:30 +0900

    Title: 십진수와 이진수 2, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240224/십진수와 이진수 2/decimal-and-binary-number-2.py
@@ -0,0 +1,17 @@
+binary = list(map(int, (list(input()))))
+
+num = 0
+for i in range(len(binary)):
+    num = num * 2 + binary[i]
+
+num *= 17
+
+digits = []
+while True:
+    if num < 2:
+        digits.append(num)
+        break
+    digits.append(num % 2)
+    num //= 2
+
+print(''.join(map(str, digits[::-1])))
