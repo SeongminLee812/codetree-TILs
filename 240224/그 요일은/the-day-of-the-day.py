721f62628ac8badb099e4ae339cb92ae2df5f285
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-24 10:47:05 +0900

    Title: 그 요일은, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240224/그 요일은/the-day-of-the-day.py
+++ 240224/그 요일은/the-day-of-the-day.py
@@ -1,26 +1,20 @@
 days_of_month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+weekday_to_num = {'Mon': 0, 'Tue': 1, 'Wed': 2, 'Thu': 3, 
+                    'Fri': 4, 'Sat': 5, 'Sun': 6}
 
 m1, d1, m2, d2 = map(int, input().split())
-count_weekday = input()
-
-# 요일 차이만큼 빼주기 위해서 요일 차이 미리 계산 
-minus = 0
-for i in range(len(weekdays)):
-    if weekdays[i] == count_weekday:
-        minus = i
+target = input()
+target_num = weekday_to_num[target]
 
 end_day = sum([days_of_month[m] for m in range(m2)]) + d2
-start_day = sum([days_of_month[m] for m in range(m1)]) + d1 + minus
-
-elapsed_day = end_day - start_day
+start_day = sum([days_of_month[m] for m in range(m1)]) + d1
 
 ans = 0
-if elapsed_day >= minus:
-    ans = elapsed_day // 7 + 1
-elif elapsed_day < 0:
-    ans = 0
-else:
-    ans = elapsed_day // 7
+cur_day = 0
+for i in range(start_day, end_day + 1):
+    if cur_day == target_num:
+        ans += 1
+    cur_day = (cur_day + 1) % 7
 
 print(ans)
