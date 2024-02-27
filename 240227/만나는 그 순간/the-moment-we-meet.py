ebdb13ab89685104badd65c11cd834ba9c70ea78
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-27 20:05:09 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240227/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,52 @@
+MAX_INT = 1000
+
+a_move = [0] * 1001
+b_move = [0] * 1001
+
+n, m = map(int, input().split())
+
+times = 0
+pos = 0
+finished_time = 0
+
+for _ in range(n):
+    direction, move = input().split()
+    move = int(move)
+    finished_time += move
+    if direction == 'R':
+        for i in range(move):
+            times += 1
+            pos += 1
+            a_move[times] = pos
+
+    else:
+        for i in range(move):
+            times += 1
+            pos -= 1
+            a_move[times] = pos
+
+# initialize
+times = 0
+pos = 0
+
+for _ in range(m):
+    direction, move = input().split()
+    move = int(move)
+    if direction == 'R':
+        for i in range(move):
+            times += 1
+            pos += 1
+            b_move[times] = pos
+
+    else:
+        for i in range(move):
+            times += 1
+            pos -= 1
+            b_move[times] = pos
+
+ans = -1
+for i in range(1, finished_time + 1):
+    if a_move[i] == b_move[i]:
+        ans = i
+        break
+print(ans)
