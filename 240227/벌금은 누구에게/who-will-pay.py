b26ed3dda6df54f713b1dcdb17c86f8fd280f8c0
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-27 20:19:54 +0900

    Title: 벌금은 누구에게, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240227/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,19 @@
+n, m, k = map(int, input().split())
+
+left = [0] + [k] * n
+
+def check():
+    for i in range(1, n):
+        if left[i] == 0:
+            return True
+    return False
+
+ans = -1
+for _ in range(m):
+    number = int(input())
+    left[number] -= 1
+    if check():
+        ans = number
+        break
+
+print(ans)
