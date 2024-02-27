dd73775407983200189680f4a9816ff58fa8f404
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-02-27 10:42:10 +0900

    Title: 계속 중첩되는 사각형, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240227/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -0,0 +1,30 @@
+OFFSET = 100
+MAX_INT = OFFSET * 2 + 1
+arr = [
+    [0] * MAX_INT
+    for _ in range(MAX_INT)
+]
+
+n = int(input())
+for i in range(n):
+    # red rectangle
+    if i % 2 == 0:
+        x1, y1, x2, y2 = map(int, input().split())
+        x1, y1, x2, y2 = x1 + OFFSET, y1 + OFFSET, x2 + OFFSET, y2 + OFFSET
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                arr[x][y] = 0
+
+    # blue rectangle
+    if i % 2 == 0:
+        x1, y1, x2, y2 = map(int, input().split())
+        x1, y1, x2, y2 = x1 + OFFSET, y1 + OFFSET, x2 + OFFSET, y2 + OFFSET
+        for x in range(x1, x2):
+            for y in range(y1, y2):
+                arr[x][y] = 1
+
+
+answer = 0
+for row in arr:
+    answer += sum(row)
+print(answer)
