4e2d3f63c14251e4e7068514b37b8bfdf4e2b3be
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-01 18:55:29 +0900

    Title: 비둘기와 전기줄, Time: 151ms, Memory: 27MB, Status: Passed - CodeTree

--- 240301/비둘기와 전기줄/pigeons-and-electric-cords.py
+++ 240301/비둘기와 전기줄/pigeons-and-electric-cords.py
@@ -10,9 +10,11 @@ for _ in range(n):
     pigeons[p].append(road)
 
 cnt = 0
-for i in range(n):
-    if len(pigeons[i]) >= 2:
-        for j in range(len(pigeons[i]) - 1):
-            if pigeons[i][j] != pigeons[i][j + 1]:
+for i in range(11):
+    pigeon = pigeons[i]
+    if len(pigeon) >= 2:
+        for j in range(len(pigeon) - 1):
+            if pigeon[j] != pigeon[j + 1]:
                 cnt += 1
+
 print(cnt)
