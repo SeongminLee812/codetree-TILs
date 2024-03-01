dd90fc3a4858ede589994174c492dfdbeff7c325
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-01 20:39:18 +0900

    Title: 연속된 숫자 만들기 2, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240301/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 240301/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,7 +1,7 @@
 a, b, c = map(int, input().split())
 
 # 셋 중 두개가 한칸만 떨어진 경우
-if (abs(a - b) == 2) and (abs(b - c) == 2):
+if (abs(a - b) == 2) or (abs(b - c) == 2):
     print(1)
 # 셋 다 붙은 경우
 elif (abs(a - b) == 1) and (abs(b - c) == 1):
