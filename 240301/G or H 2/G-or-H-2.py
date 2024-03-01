ca3bf87032751239f1b785fe1387da0a13db0ad8
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-01 10:31:22 +0900

    Title: G or H 2, Time: 159ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240301/G or H 2/G-or-H-2.py
@@ -0,0 +1,36 @@
+# search starting point i
+# search district range j
+
+n = int(input())
+arr = [0] * (100 + 1)
+
+max_position = 0
+
+for _ in range(n):
+    pos, alpha = input().split()
+    pos = int(pos)
+    arr[pos] = alpha
+    max_position = max(max_position, pos)
+
+ans = 0
+
+for i in range(max_position):
+    for j in range(1, max_position - i):
+        g_count = 0
+        h_count = 0
+        district = arr[i: i + j + 1]
+        if district[0] == 0 or district[-1] == 0:
+            continue
+        for d in district:
+            if d == 'G':
+                g_count += 1
+            elif d == 'H':
+                h_count += 1
+        if (g_count == h_count):
+            ans = max(ans, j)
+        if not h_count:
+            ans = max(ans, j)
+        if not g_count:
+            ans = max(ans, j)
+
+print(ans)
