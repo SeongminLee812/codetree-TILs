ff82824f8c8d5cda977fd7d64e460f74a814d53d
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-03 21:26:54 +0900

    Title: ABC 줄 세우기, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240303/ABC 줄 세우기/abc-line-up.py
+++ 240303/ABC 줄 세우기/abc-line-up.py
@@ -4,6 +4,7 @@ arr = list(input().split())
 
 for i in range(n):
     arr[i] = ord(arr[i]) - ord('A')
+print(arr)
 
 def is_sorted():
     for i in range(n):
@@ -11,28 +12,12 @@ def is_sorted():
             return False
     return True
 
-# 정렬 방법
-# 가장 작은 원소 먼저 찾음
-# 가장 작은 원소의 왼쪽과 차례로 비교해가며 swap
+ans = 0
+while not is_sorted():
+    for j in range(n - 1, 0, -1):
+        if arr[j] < arr[j - 1]:
+            arr[j], arr[j - 1] = arr[j - 1], arr[j]
+            ans += 1
 
-def selection_sort():
-    cnt = 0
-    min_idx = 0
-    min_val = 27
-    sorted_list = [False] * n
 
-    while not is_sorted():
-        # for i in range(n):
-        #     if not sorted_list[i] and arr[i] < min_val:
-        #         min_idx = i
-        #         min_val = arr[i]
-
-        for j in range(n - 1, 0, -1):
-            if arr[j] < arr[j - 1]:
-                arr[j], arr[j - 1] = arr[j - 1], arr[j]
-                cnt += 1
-
-    return cnt
-
-ans = selection_sort()
 print(ans)
