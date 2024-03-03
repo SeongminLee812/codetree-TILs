67c1892d296f93e62bd13a56493015e88251d373
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-03 14:10:41 +0900

    Title: 연속된 숫자 만들기 3, Time: 132ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240303/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -0,0 +1,20 @@
+arr = list(map(int, input().split()))
+
+longest_term = 0
+
+for i in range(len(arr)):
+    if i == 0:
+        continue
+    term = arr[i] - arr[i - 1]
+    longest_term = max(longest_term, term)
+
+if longest_term == 1:
+    print(0)
+elif longest_term == 2:
+    print(1)
+else:
+    cnt = 1
+    while longest_term > 1:
+        longest_term //= 2
+        cnt += 1
+    print(cnt)
