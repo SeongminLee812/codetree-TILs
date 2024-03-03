f8c91c6edac5867e1fa611610b460a4dc889ccc4
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-03 12:34:27 +0900

    Title: 코딩톡, Time: 120ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240303/코딩톡/coding-talk.py
+++ 240303/코딩톡/coding-talk.py
@@ -1,17 +1,58 @@
 n, m, p = map(int, input().split())
 
-arr = [1] * n
+# 메세지 별로 안읽어도 되는 사람을 기록/ 행 : 메세지, 열: 사람
+can_unread =[
+    [i for i in range(n)]
+    for _ in range(m)
+]
 
-for i in range(1, m + 1):
+# result array / 1: 안읽을 가능성이 있는 사람, 0: 읽어야만 하는 사람
+arr = [0] * n
+
+# 메세지를 보낸 사람이 누구인지 체크
+senders = [0] * m
+
+# 각 메세지 별로 읽은 사람의 수를 기록하는 array
+read_people = [0] * m
+
+for i in range(m):
     person, nums = input().split()
     nums = int(nums)
-    if i >= p:
-        person = ord(person) - ord('A')
-        arr[person] = 0
+    read_people[i] = nums
+    person_num = ord(person) - ord('A')
 
-    if i == p and nums == 0:
-        for j in range(n):
-            arr[j] = 0
+    senders[i] = person_num
+    # 안읽은 사람이 0인 경우 전부 제거
+    if nums == 0:
+        can_unread[i] = []
+
+    # 이전 메세지에서 안읽어도 되는 사람 다 제거
+    else:
+        for prev_m in range(i + 1):
+            try:
+                can_unread[prev_m].remove(person_num)
+            except:
+                pass
+
+
+for message_num in range(m):
+    if message_num == 0:
+        continue
+    if read_people[message_num - 1] == read_people[message_num]:
+        try:
+            sender = senders[message_num - 1]
+            # print(f'message: {message_num}, person: {sender}')
+            can_unread[message_num].remove(sender) # 여기에 i - 1번째 메세지를 보낸 사람을 찾아서 제거
+        except:
+            pass
+
+#
+# for row in can_unread:
+#     print(row)
+
+# 정답 찾기
+for person_num in can_unread[p - 1]:
+    arr[person_num] = 1
 
 for i in range(n):
     if arr[i]:
