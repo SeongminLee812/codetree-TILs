fb691a9a9aaac593d945619d7d6c079d37caa1b5
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-04 11:48:23 +0900

    Title: 순위 경쟁, Time: 159ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/순위 경쟁/ranking-competition.py
+++ 240304/순위 경쟁/ranking-competition.py
@@ -4,24 +4,26 @@ n = int(input())
 
 def who_first(scores):
     A, B, C = scores['A'], scores['B'], scores['C']
+    return_val = 0
 
-    # 단독 1등
-    if A > B and A > C:
-        return 'A'
-    elif B > C and B > A:
-        return 'B'
-    elif C > A and C > B:
-        return 'C'
-    # 두명이 1등
-    elif A > B and A == C:
-        return 'AC'
-    elif B > A and B == C:
-        return 'BC'
-    elif A > C and A == B:
-        return 'AB'
-    # 세명이 1등
-    elif A == B == C:
-        return 'ABC'
+    """
+    A만 올라간 경우 1
+    B만 올라간 경우 2
+    C만 올라간 경우 4
+    AB 올라간 경우 3
+    AC 올라간 경우 5
+    BC 올라간 경우 6
+    ABC 올라간 경우 7 
+    """
+    max_val = max(A, B, C)
+    if max_val == A:
+        return_val += 1
+    if max_val == B:
+        return_val += 2
+    if max_val == C:
+        return_val += 4
+
+    return return_val
 
 score_dict = {'A': 0, 'B': 0, 'C': 0}
 prev_dict = {'A': 0, 'B': 0, 'C': 0}
