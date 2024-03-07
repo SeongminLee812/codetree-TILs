08cd8fa1ab12fbf3df8af60163cf8b85daca59b6
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-07 19:10:27 +0900

    Title: 움직이는 블록, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240307/움직이는 블록/moving-block.py
+++ 240307/움직이는 블록/moving-block.py
@@ -1,15 +1,23 @@
-# 좋지 않은 선택
-# 1. 이미 작은 수의 블럭을 다른 블럭으로 옮기는 것
-# 따라서 이미 큰 수의 블럭을 가장 작은 곳으로 옯긴다.
-# 최선의 선택 -> 가장 큰 놈을 옮기는 것
-# 어떻게 ?
 import sys
 
+n = int(input())
 
-# 제일 큰놈에서 제일 작은 놈으로 옮기되 차이만큼 한번에 덜어줘야 복잡도를 줄일 수 있음
-# 종료 조건 : 가장 큰 n이 다른 수와 동일해질 때까지
+arr = [
+    int(input())
+    for _ in range(n)
+]
 
-def find_min_max():
+def find_second(max_val):
+    second_val = 0
+    for i in range(n):
+        if arr[i] < max_val and arr[i] > second_val:
+            second_val = arr[i]
+    try:
+        return second_val
+    except:
+        return False
+
+def find_max():
     max_val = 0
     min_val = sys.maxsize
     max_idx = 0
@@ -23,25 +31,23 @@ def find_min_max():
             min_val = arr[i]
             min_idx = i
 
-    return min_val, min_idx, max_val, max_idx
-
-n = int(input())
-arr = [
-    int(input())
-    for _ in range(n)
-]
-
+    if max_val == min_val:
+        return False
+    return max_val, max_idx, min_val, min_idx
 
 cnt = 0
-for i in range(n):
-    min_val, min_idx, max_val, max_idx = find_min_max()
-    diff = max_val - min_val
-    if diff == 0:
+while True:
+    value_tuple = find_max()
+    if not value_tuple:
         break
+    else:
+        max_val, max_idx, min_val, min_idx = value_tuple
 
-    diff //= 2
-    arr[max_idx] = max_val - diff
-    arr[min_idx] = min_val + diff
+    second_val = find_second(max_val)
+    diff = max_val - second_val
+    arr[min_idx] += diff
+    arr[max_idx] -= diff
     cnt += diff
+    print(arr)
 
 print(cnt)
