079bf1eae52071c08cf612201e5a58d9cb8732e0
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-08 21:47:58 +0900

    Title: 전부 포함하는 선분, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240308/전부 포함하는 선분/all-inclusive-segment.py
+++ 240308/전부 포함하는 선분/all-inclusive-segment.py
@@ -1,25 +1,17 @@
 n = int(input())
 
-min_idx = 0
-min_val = 101
-second_min_val = 101
-
-max_idx = 0
-max_val = 0
-second_max_val = 0
+x1_list = []
+x2_list = []
 
 for i in range(n):
     x1, x2 = map(int, input().split())
-    if x1 < min_val:
-        min_val = x1
-        min_idx = i
-    elif x1 < second_min_val:
-        second_min_val = x1
-    if x2 > max_val:
-        max_val = x2
-        max_idx = i
-    elif x2 > second_max_val:
-        second_max_val = x2
+    x1_list.append(x1)
+    x2_list.append(x2)
+
+x1_list.sort()
+x2_list.sort(key=lambda x: -x)
+min_val, second_min_val = x1_list[0], x1_list[1]
+max_val, second_max_val = x2_list[0], x2_list[1]
 
 candidates = [second_max_val - min_val,
               max_val - second_min_val]
