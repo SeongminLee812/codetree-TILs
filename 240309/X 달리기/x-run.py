11128874f3bd27b9373b6b9f8be2b8c5fde252bc
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-09 19:18:15 +0900

    Title: X 달리기, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/X 달리기/x-run.py
+++ 240309/X 달리기/x-run.py
@@ -1,35 +1,24 @@
-import sys
-
 x = int(input())
-
-if x == 1:
-    print(1)
-    sys.exit()
-
-prev_vel = 1
-cur_pos = 0
-elapsed = 0
-
-go_on = True
-
-def sigma(n):
-    return (n * (n + 1)) // 2
+t = 0
+left_dist = x
+v = 1
 
 while True:
-    if cur_pos == x:
+    left_dist -= v
+    t += 1
+    if left_dist == 0:
         break
-    cur_pos += prev_vel
-    if go_on and cur_pos + sigma(prev_vel + 1) <= x:
-        cur_vel = prev_vel + 1
 
-    # 한번이라도 넘어버리면 그 이후에는 감소 or 유지 밖에 없음
-    # 넘는 경우
-    if cur_pos + sigma(cur_vel) > x:
-        go_on = False
-        cur_vel = prev_vel - 1
+    # 속도를 증가시킬 경우 이동거리
+    if left_dist >= (v + 1) * (v + 2) // 2:
+        v += 1
 
-    # 유지하는 경우
-    elapsed += 1
-    prev_vel = cur_vel
+    # 속도를 유지할 경우 이동거리 고려
+    elif left_dist >= v * (v + 1) // 2:
+        v = v
 
-print(elapsed)
+    # 속도를 증가시키거나 유지하면 target을 넘어가버리는 경우 속도를 줄여야함.
+    else:
+        v -= 1
+
+print(t)
