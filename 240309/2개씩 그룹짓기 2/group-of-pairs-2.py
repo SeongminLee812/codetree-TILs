500a96522bade308c9ca7533237cb394597b1a6f
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-09 13:53:22 +0900

    Title: 2개씩 그룹짓기 2, Time: 191ms, Memory: 40MB, Status: Passed - CodeTree

---
+++ 240309/2개씩 그룹짓기 2/group-of-pairs-2.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(input())
+
+arr = list(map(int, input().split()))
+arr.sort()
+
+ans = sys.maxsize
+for i in range(n):
+    diff = arr[i + n] - arr[i]
+    ans = min(ans, diff)
+
+print(ans)
