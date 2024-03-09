219ac88b44633c7bf5021b90706ded1b69750a6a
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-09 15:52:58 +0900

    Title: X 달리기, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

--- 240309/X 달리기/x-run.py
+++ 240309/X 달리기/x-run.py
@@ -1,23 +1,35 @@
+import sys
+
 x = int(input())
 
-cur_vel = 1
-cur_pos = 1
-elapsed = 1
+if x == 1:
+    print(1)
+    sys.exit()
+
+prev_vel = 1
+cur_pos = 0
+elapsed = 0
+
+go_on = True
 
 def sigma(n):
     return (n * (n + 1)) // 2
 
 while True:
-    if cur_pos + sigma(cur_vel) < x: # 속력에 1 더했을 때 넘지 않는다면
-        cur_vel += 1
-        cur_pos += cur_vel
-        elapsed += 1
-    elif cur_pos + sigma(cur_vel) == x:
-        elapsed += cur_vel
+    if cur_pos == x:
         break
-    elif cur_pos + sigma(cur_vel) > x: # 넘으면
-        cur_vel -= 1
-        cur_pos += cur_vel
-        elapsed += 1
+    cur_pos += prev_vel
+    if go_on and cur_pos + sigma(prev_vel + 1) <= x:
+        cur_vel = prev_vel + 1
+
+    # 한번이라도 넘어버리면 그 이후에는 감소 or 유지 밖에 없음
+    # 넘는 경우
+    if cur_pos + sigma(cur_vel) > x:
+        go_on = False
+        cur_vel = prev_vel - 1
+
+    # 유지하는 경우
+    elapsed += 1
+    prev_vel = cur_vel
 
 print(elapsed)
