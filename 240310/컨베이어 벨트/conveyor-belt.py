cc9cf287bd0ec9d9f62e8bb7c8951e204ab7767c
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-10 11:36:08 +0900

    Title: 컨베이어 벨트, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240310/컨베이어 벨트/conveyor-belt.py
@@ -0,0 +1,8 @@
+C:\Users\82103\anaconda3\envs\coding_test\python.exe C:\Users\82103\DS_workspaces\coding_test\codetree\삼성_빠른완료테스트.py 
+3 2
+1 2 3
+6 5 4
+5 4 1
+2 3 6
+
+Process finished with exit code 0
