e464a42bb9b0b15fd17de514fc9ed17b26ea4ece
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-10 11:36:16 +0900

    Title: 컨베이어 벨트, Time: 168ms, Memory: 25MB, Status: Passed - CodeTree

--- 240310/컨베이어 벨트/conveyor-belt.py
+++ 240310/컨베이어 벨트/conveyor-belt.py
@@ -1,8 +1,39 @@
-C:\Users\82103\anaconda3\envs\coding_test\python.exe C:\Users\82103\DS_workspaces\coding_test\codetree\삼성_빠른완료테스트.py 
-3 2
-1 2 3
-6 5 4
-5 4 1
-2 3 6
+n, t = map(int, input().split())
 
-Process finished with exit code 0
+arr = [
+    list(map(int, input().split()))
+    for _ in range(2)
+]
+
+# 두번째 줄 뒤집기
+new_arr = [0] * n
+for i in range(n):
+    new_arr[i] = arr[1][n - 1 - i]
+arr[1] = new_arr
+
+for _ in range(t):
+
+    # 첫번째 행은 밀어주기
+    first_row_key = arr[0][n - 1]
+
+    for i in range(n - 1, 0, -1):
+        arr[0][i] = arr[0][i - 1]
+
+    # 두번째 행은 당겨주기
+    second_row_key = arr[1][0]
+
+    for j in range(n - 1):
+        arr[1][j] = arr[1][j + 1]
+
+    # 빈자리 매꿔주기
+    arr[1][n - 1] = first_row_key
+    arr[0][0] = second_row_key
+
+# 두번째 줄 뒤집기
+new_arr = [0] * n
+for i in range(n):
+    new_arr[i] = arr[1][n - 1 - i]
+arr[1] = new_arr
+
+for line in arr:
+    print(' '.join(map(str, line)))
