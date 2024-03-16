dcb667d52cdeb683e5339d91c7ad7b73c5c77008
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-16 13:47:06 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240316/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -0,0 +1,60 @@
+n = int(input())
+x, y = map(int, input().split())
+arr = [
+    list(input())
+    for _ in range(n)
+]
+
+def out_of_grid(x, y):
+    return x < 0 or x >= n or y < 0 or y >= n
+
+def is_wall(x, y):
+    wall_dir = (dir_num + 1) % 4
+    wall_x = x + dx[wall_dir]
+    wall_y = y + dy[wall_dir]
+    if arr[wall_x][wall_y] == '#':
+        return True
+    return False
+
+t = 0
+x -= 1
+y -= 1
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+dir_num = 0
+
+visited = [
+    [False] * n
+    for _ in range(n)
+]
+visited[x][y] = True
+
+while True:
+    if is_wall(x, y):
+        nx, ny = x + dx[dir_num], y + dy[dir_num]
+        if out_of_grid(nx, ny):
+            t += 1
+            break
+        elif arr[nx][ny] == '#':
+            dir_num = (dir_num - 1 + 4) % 4
+        elif arr[nx][ny] == '.':
+            # 이동하려한 공간이 이미 방문한 곳일 때(탈출 불가)
+            if visited[nx][ny]:
+                t = -1
+                break
+            # 방문한 적이 없다면
+            visited[nx][ny] = True
+            x = nx
+            y = ny
+            t += 1
+
+    else:
+        dir_num = (dir_num + 1) % 4
+        nx = x + dx[dir_num]
+        ny = y + dy[dir_num]
+        x = nx
+        y = ny
+        t += 1
+
+print(t)
