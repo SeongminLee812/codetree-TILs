25a6f10ba50b7ccab0cf99d5ecb9c0947ea338c2
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-16 14:18:13 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 115ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240316/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 240316/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -24,16 +24,20 @@ dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 dir_num = 0
 
-visited = [
-    [False] * n
+visited =[
+    [
+        [False] * 4
+        for _ in range(n)
+    ]
     for _ in range(n)
 ]
-visited[x][y] = True
-
-left_turn = 0
-right_turn = 0
 
 while True:
+    if visited[x][y][dir_num]:
+        t = -1
+        break
+    visited[x][y][dir_num] = True
+
     if is_wall(x, y):
         nx, ny = x + dx[dir_num], y + dy[dir_num]
         if out_of_grid(nx, ny):
@@ -41,32 +45,16 @@ while True:
             break
         elif arr[nx][ny] == '#':
             dir_num = (dir_num - 1 + 4) % 4
-            left_turn += 1
-            right_turn = 0
         elif arr[nx][ny] == '.':
-            # 이동하려한 공간이 이미 방문한 곳일 때(탈출 불가)
-            # if visited[nx][ny]:
-            #     t = -1
-            #     break
-            # 방문한 적이 없다면
-            visited[nx][ny] = True
-            x = nx
-            y = ny
-            t += 1
-
-    else:
-        dir_num = (dir_num + 1) % 4
-        left_turn = 0
-        right_turn += 1
-        nx = x + dx[dir_num]
-        ny = y + dy[dir_num]
-        x = nx
-        y = ny
-        t += 1
-
-    if t > 10000 or left_turn > 4 or right_turn > 4:
-        t = -1
-        break
-        
-
+            if is_wall(nx, ny):
+                x = nx
+                y = ny
+                t += 1
+            else:
+                dir_num = (dir_num + 1) % 4
+                nx = x + dx[dir_num]
+                ny = y + dy[dir_num]
+                x = nx
+                y = ny
+                t += 2
 print(t)
