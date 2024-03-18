60b3a45c7b096e8842620622710464e48a5e79f8
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-18 21:50:43 +0900

    Title: 1차원 윷놀이, Time: 125ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240318/1차원 윷놀이/yutnori-1d.py
+++ 240318/1차원 윷놀이/yutnori-1d.py
@@ -8,29 +8,31 @@ move_each_turn = list(map(int, input().split()))
 
 def calc_score():
     total_pass = 0
-    score_board = [1] * (k + 1)
 
-    for i in range(n):
-        score_board[knights[i]] += move_each_turn[i]
-    for i in range(1, k + 1):
+    for i in range(k):
         if score_board[i] >= m:
             total_pass += 1
     return total_pass
 
 def choose_knight(curr_index):
     global ans
-    if curr_index == n + 1:
+    if curr_index == n:
         score = calc_score()
         ans = max(ans, score)
         return
 
-    for i in range(1, k + 1):
+    for i in range(k):
+        if score_board[i] >= m:
+            continue
         knights.append(i)
+        score_board[i] += move_each_turn[curr_index]
         choose_knight(curr_index + 1)
+        score_board[i] -= move_each_turn[curr_index]
         knights.pop()
 
 knights = []
 ans = 0
+score_board = [1] * k
 
-choose_knight(1)
+choose_knight(0)
 print(ans)
