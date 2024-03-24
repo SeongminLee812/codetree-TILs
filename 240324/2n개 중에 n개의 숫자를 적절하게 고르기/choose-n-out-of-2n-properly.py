f93c1869936396404d87229664f751af58e01c7b
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-24 17:36:58 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 159ms, Memory: 26MB, Status: Passed - CodeTree

--- 240324/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 240324/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -7,7 +7,7 @@ total = sum(arr)
 group = []
 ans = sys.maxsize
 
-def choose(num_index, cnt):
+def choose(num_index, cnt, group_a):
     """
     현재까지 cnt개를 선택했을 때, arr에서 num_index 번째 숫자를 선택할 지 선택하지 않을 지 결정하는 함수
     :param num_index:
@@ -16,7 +16,6 @@ def choose(num_index, cnt):
     """
     global ans
     if cnt == n:
-        group_a = sum(group)
         group_b = total - group_a
         diff = abs(group_b - group_a)
         ans = min(ans, diff)
@@ -25,12 +24,9 @@ def choose(num_index, cnt):
         return
 
     # 선택
-    group.append(arr[num_index])
-    choose(num_index + 1, cnt + 1)
-    group.pop()
-
+    choose(num_index + 1, cnt + 1, group_a + arr[num_index])
     # 미선택
-    choose(num_index + 1, cnt)
+    choose(num_index + 1, cnt, group_a)
 
-choose(0, 0)
+choose(0, 0, 0)
 print(ans)
