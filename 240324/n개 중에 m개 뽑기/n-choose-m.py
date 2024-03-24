e4832558819ab2cd6dfa1a739ff84e3dceb92f35
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-24 16:54:51 +0900

    Title: n개 중에 m개 뽑기, Time: 151ms, Memory: 25MB, Status: Passed - CodeTree

--- 240324/n개 중에 m개 뽑기/n-choose-m.py
+++ 240324/n개 중에 m개 뽑기/n-choose-m.py
@@ -1,16 +1,32 @@
 n, m = map(int, input().split())
 
-ans = []
+arr = [False] * (n + 1)
 
-def choose(curr_num, start_num):
-    if curr_num == m + 1:
-        print(' '.join(map(str, ans)))
+def print_answer():
+    for i in range(1, n + 1):
+        if arr[i]:
+            print(i, end=' ')
+    print()
+
+def choose(num_index, selected_cnt):
+    """
+    숫자 후보 중 num_index 번째의 숫자를 뽑을 지 말지 결정하는 함수
+    m개 만큼 뽑으면 종료
+    :param num_index: 주어진 숫자 후보 중 몇번 째 숫자를 고를 건지 말건지
+    :param selected_cnt: 현재까지 선택된 수의 개수
+    :return:
+    """
+    if selected_cnt == m:
+        print_answer()
+        return
+    if num_index == n + 1:
         return
 
-    for i in range(start_num, n + 1):
-        ans.append(i)
-        choose(curr_num + 1, i + 1)
-        ans.pop()
+    arr[num_index] = True
+    choose(num_index + 1, selected_cnt + 1)
+    arr[num_index] = False
+
+    choose(num_index + 1, selected_cnt)
 
 
-choose(1, 1)
+choose(1, 0)
