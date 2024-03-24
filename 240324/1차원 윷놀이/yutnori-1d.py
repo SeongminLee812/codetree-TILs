9a069863f746e10f7e6e9efed873ae3a181ff431
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-24 15:43:04 +0900

    Title: 1차원 윷놀이, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240324/1차원 윷놀이/yutnori-1d.py
@@ -0,0 +1,30 @@
+n, m, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+score_board = [1] * k
+def calc_score():
+    cnt = 0
+    for i in range(k):
+        if score_board[i] >= m:
+            cnt += 1
+    return cnt
+
+
+ans = 0
+
+def choose(curr_num):
+    global ans
+    if curr_num == n:
+        cnt = calc_score()
+        ans = max(cnt, ans)
+        return
+
+    for i in range(k):
+        if score_board[i] >= m:
+            continue
+        score_board[i] += arr[curr_num]
+        choose(curr_num + 1)
+        score_board[i] -= arr[curr_num]
+
+choose(0)
+print(ans)
