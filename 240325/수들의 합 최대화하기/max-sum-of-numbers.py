023e52a331572bd60d370e4d11dc8dcf021eb0c5
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-25 22:15:31 +0900

    Title: 수들의 합 최대화하기, Time: 5086ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

--- 240325/수들의 합 최대화하기/max-sum-of-numbers.py
+++ 240325/수들의 합 최대화하기/max-sum-of-numbers.py
@@ -4,43 +4,52 @@ arr = [
     for _ in range(n)
 ]
 
-picked = []
+picked_row = []
+picked_col = []
 
 visited_row = [False] * n
 visited_col = [False] * n
 
 def calc():
     value = 0
-    for i, j in picked:
+    for i, j in zip(picked_row, picked_col):
         value += arr[i][j]
     return value
 
-def choose(cnt):
+def choose(row_cnt, col_cnt):
     # picked의 cnt번째 수를 결정하는 함수
     global ans
+    combi = 0
 
-    if cnt == n + 1:
+    if row_cnt == col_cnt == n + 1:
         ans = max(ans, calc())
+        combi += 1
         return
 
-    for i in range(n):
-        if visited_row[i]:
-            continue
-        visited_row[i] = True
+    if row_cnt <= n:
+        for i in range(n):
+            if visited_row[i]:
+                continue
+            visited_row[i] = True
+            picked_row.append(i)
+            choose(row_cnt + 1, col_cnt)
 
+            picked_row.pop()
+            visited_row[i] = False
+
+    else:
         for j in range(n):
             if visited_col[j]:
                 continue
-
             visited_col[j] = True
-            picked.append((i, j))
-
-            choose(cnt + 1)
+            picked_col.append(j)
+            choose(row_cnt, col_cnt + 1)
+            picked_col.pop()
             visited_col[j] = False
-            picked.pop()
 
-        visited_row[i] = False
+
+
 
 ans = 0
-choose(1)
+choose(1, 1)
 print(ans)
