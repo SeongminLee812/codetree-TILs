f5383e3c3c1b659e113b300f06e3910faec3bf43
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-27 21:51:40 +0900

    Title: 외판원 순회, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/외판원 순회/traveling-salesman-problem.py
@@ -0,0 +1,40 @@
+import sys
+
+n = int(input())
+arr = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+visited = [False] * n
+path = [0]
+
+def calc_cost(path):
+    local_path = path[:] + [0]
+    total = 0
+    prev_i = 0
+    for i in range(1, n + 1):
+        value = arr[local_path[prev_i]][local_path[i]]
+        total += value
+        prev_i = i
+    return total
+
+def choose(curr_index):
+    global ans
+    if curr_index == n - 1:
+        ans = min(ans, calc_cost(path))
+        return
+
+    for i in range(1, n):
+        if visited[i]:
+            continue
+        visited[i] = True
+        path.append(i)
+
+        choose(curr_index + 1)
+        visited[i] = False
+        path.pop()
+
+ans = sys.maxsize
+choose(0)
+print(ans)
