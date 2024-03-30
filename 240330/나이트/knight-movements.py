0bea1abc2891e8d73027047d6637c697f5b9b190
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-03-30 21:26:47 +0900

    Title: 나이트, Time: 142ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240330/나이트/knight-movements.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+n = int(input())
+r1, c1, r2, c2 = map(int, input().split())
+
+visited = [
+    [False] * n
+    for _ in range(n)
+]
+step = [
+    [0] * n
+    for _ in range(n)
+]
+
+q = deque()
+q.append((r1 - 1, c1 - 1))
+visited[r1 - 1][c1 - 1] = True
+
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+def can_go(x, y):
+    if not in_range(x, y) or visited[x][y]:
+        return False
+    return True
+
+def push(x, y, s):
+    visited[x][y] = True
+    step[x][y] = s
+    q.append((x, y))
+
+def bfs():
+    dxs = [-2, -2, -1, 1, 2, 2, 1, -1]
+    dys = [-1, 1, 2, 2, 1, -1, -2, -2]
+
+    while q:
+        x, y = q.popleft()
+        for dx, dy in zip(dxs, dys):
+            nx, ny = x + dx, y + dy
+            if can_go(nx, ny):
+                push(nx, ny, step[x][y] + 1)
+
+bfs()
+
+end = step[r2 - 1][c2 - 1]
+if end:
+    print(end)
+else:
+    print(-1)
