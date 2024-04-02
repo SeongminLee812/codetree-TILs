3bf9c43303b1b66af2ae9a39ec224a8108c9eec8
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-02 14:29:51 +0900

    Title: 알파벳과 사칙연산, Time: 159ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240402/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -0,0 +1,35 @@
+arr = input()
+
+def calc(num_list):
+    alpha_list = ['a', 'b', 'c', 'd', 'e', 'f']
+    alpha_dict = dict(zip(alpha_list, num_list))
+
+    value = alpha_dict[arr[0]]
+
+    for i in range(1, len(arr), 2):
+        if arr[i] == '*':
+            value *= alpha_dict[arr[i + 1]]
+        elif arr[i] == '-':
+            value -= alpha_dict[arr[i + 1]]
+        elif arr[i] == '+':
+            value += alpha_dict[arr[i + 1]]
+    return value
+
+
+def go(curr_index_in_ans):
+    global ans
+    if curr_index_in_ans == 6:
+        value = calc(nums)
+        ans = max(value, ans)
+        return
+
+    for i in range(1, 5):
+        nums.append(i)
+        go(curr_index_in_ans + 1)
+        nums.pop()
+
+ans = 0
+nums = []
+
+go(0)
+print(ans)
