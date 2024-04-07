64defc5214c0115d1994462a59c5b344c19ccc16
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-07 16:40:20 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 122ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240407/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240407/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,5 +1,6 @@
 import sys
-from collections import deque
+INT_MIN = -sys.maxsize
+sys.setrecursionlimit(10)
 
 n = int(input())
 
@@ -7,10 +8,14 @@ arr = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
-INT_MIN = -sys.maxsize
 
-dp = [
+memo = [
     [INT_MIN] * n
+    for _ in range(n)
+]
+
+visited = [
+    [False] * n
     for _ in range(n)
 ]
 
@@ -18,47 +23,36 @@ def in_range(x, y):
     return x >= 0 and x < n and y >= 0 and y < n
 
 def is_start(x, y):
-    # 시작점인지 판단
-    # 시작점 정의 : 다른 인접한 칸으로부터 흘러들어올 여지가 없는 포인트
     dxs = [-1, 1, 0, 0]
     dys = [0, 0, -1, 1]
     for dx, dy in zip(dxs, dys):
         nx, ny = x + dx, y + dy
-        if in_range(nx, ny) and arr[nx][ny] < arr[x][y]:
+        if in_range(nx, ny) and arr[nx][ny] > arr[x][y]:
             return False
     return True
 
-def can_go(x, y, threshold):
-    if not in_range(x, y):
-        return False
-    if arr[x][y] <= threshold:
-        return False
-    return True
+def get_max(x, y, threshold):
+    if not in_range(x, y) or arr[x][y] > threshold:
+        return INT_MIN
 
-def push(x, y, s):
-    dp[x][y] = s
-    q.append((x, y))
+    if visited[x][y]:
+        return memo[x][y]
 
-q = deque()
+    if is_start(x, y):
+        memo[x][y] = 1
+        return memo[x][y]
+
+    if memo[x][y] != INT_MIN:
+        return memo[x][y]
+
+    t = arr[x][y]
+    memo[x][y] = max(get_max(x - 1, y, t), get_max(x + 1, y, t), get_max(x, y - 1, t), get_max(x, y + 1, t)) + 1
+
+    return memo[x][y]
 
 for i in range(n):
     for j in range(n):
-        if is_start(i, j):
-            dp[i][j] = 1
-            q.append((i, j))
+        visited[i][j] = True
+        get_max(i, j, arr[i][j])
 
-dxs = [-1, 1, 0, 0]
-dys = [0, 0, -1, 1]
-
-while q:
-    x, y = q.popleft()
-    for dx, dy in zip(dxs, dys):
-        nx, ny = x + dx, y + dy
-        if can_go(nx, ny, arr[x][y]):
-            push(nx, ny, dp[x][y] + 1)
-
-
-max_val = 0
-for line in dp:
-    max_val = max(max_val, max(line))
-print(max_val)
+print(memo)
