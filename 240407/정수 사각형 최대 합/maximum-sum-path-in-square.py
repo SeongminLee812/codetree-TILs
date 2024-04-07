e5b11fcc62e743d17f9eda69505d92adaa3bf8fd
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-07 09:02:46 +0900

    Title: 정수 사각형 최대 합, Time: 168ms, Memory: 26MB, Status: Passed - CodeTree

--- 240407/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240407/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -6,24 +6,28 @@ arr = [
 ]
 
 # initialize
-dp = [
-    [0] * n
+memo = [
+    [-1] * n
     for _ in range(n)
 ]
 
-dp[0][0] = arr[0][0]
+memo[0][0] = arr[0][0]
 
 for i in range(1, n):
-    dp[i][0] = dp[i - 1][0] + arr[i][0]
-    dp[0][i] = dp[0][i - 1] + arr[0][i]
+    memo[i][0] = memo[i - 1][0] + arr[i][0]
+    memo[0][i] = memo[0][i - 1] + arr[0][i]
 
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
 
-ans = dp[0][0]
+def get_sum(x, y):
+    if not in_range(x, y):
+        return
+    if memo[x][y] != -1:
+        return memo[x][y]
+    else:
+        memo[x][y] = max(get_sum(x - 1, y), get_sum(x, y - 1)) + arr[x][y]
+    return memo[x][y]
 
-for i in range(1, n):
-    for j in range(1, n):
-        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + arr[i][j]
-        if dp[i][j] > ans:
-            ans = dp[i][j]
-
-print(ans)
+get_sum(n - 1, n - 1)
+print(memo[n - 1][n - 1])
