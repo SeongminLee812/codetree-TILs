2c4b12a8ba63adf025485da5be5e13297cd17607
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-09 21:31:22 +0900

    Title: 정수 사각형 최장 증가 수열, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240409/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240409/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -1,23 +1,30 @@
 import heapq
+import sys
+sys.setrecursionlimit(250001)
 
-# with open('input.txt', 'r') as f:
-#     n = int(f.readline())
+with open('input.txt', 'r') as f:
+    n = int(f.readline())
+
+    arr = [
+        list(map(int, f.readline().split()))
+        for _ in range(n)
+    ]
+
+
+# n = int(input())
 #
-#     arr = [
-#         list(map(int, f.readline().split()))
-#         for _ in range(n)
-#     ]
-
-
-n = int(input())
-
-arr = [
-    list(map(int, input().split()))
-    for _ in range(n)
-]
+# arr = [
+#     list(map(int, input().split()))
+#     for _ in range(n)
+# ]
 
 memo = [
     [0] * n
+    for _ in range(n)
+]
+
+visited = [
+    [False] * n
     for _ in range(n)
 ]
 
@@ -27,7 +34,7 @@ def in_range(x, y):
 def can_go(x, y, t, step):
     if not in_range(x, y):
         return False
-    if memo[x][y] >= step:
+    if visited[x][y]:
         return False
     if arr[x][y] <= t:
         return False
@@ -43,6 +50,7 @@ def get_max(x, y, step):
 
         # 오른차순 정렬 필요 -> 작은 수 부터 방문
         if can_go(nx, ny, arr[x][y], step):
+            visited[nx][ny] = True
             heapq.heappush(_heap, (arr[nx][ny], nx, ny)) # 이때 이미 넣어놈
 
     # print('now :', x, y,)
@@ -63,6 +71,7 @@ for i in range(n):
 
 for _, i, j in q:
     if memo[i][j] == 0:
+        visited[i][j] = True
         memo[i][j] = 1
         get_max(i, j, 2)
 
