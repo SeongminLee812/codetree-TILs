d1137216f0b13a24c80333dd2d106abf3e0a6f18
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-09 22:05:45 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 1126ms, Memory: 70MB, Status: Passed - CodeTree

--- 240409/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240409/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -11,31 +11,34 @@ for i in range(n):
 q.sort()
 
 dp = [
-    [0] * n
+    [-1] * n
     for _ in range(n)
 ]
-
-def in_range(x, y):
-    return x >= 0 and x < n and y >= 0 and y < n
-
-def can_go(x, y, t):
-    if not in_range(x, y):
-        return False
-    if arr[x][y] >= t:
-        return False
-    return True
-
 
 dxs = [-1, 1, 0, 0]
 dys = [0, 0, -1, 1]
 
-for value, x, y in q:
-    max_val = 0
+def in_range(x, y):
+    return x >= 0 and x < n and y >= 0 and y < n
+
+
+def find_max(x, y):
+    if dp[x][y] != -1:
+        return dp[x][y]
+
+    best = 1
+
     for dx, dy in zip(dxs, dys):
         nx, ny = x + dx, y + dy
-        if can_go(nx, ny, arr[x][y]):
-            max_val = max(max_val, dp[nx][ny])
-    dp[x][y] = max_val + 1
+        if in_range(nx, ny) and arr[nx][ny] < arr[x][y]:
+            best = max(best, find_max(nx, ny) + 1)
+
+    dp[x][y] = best
+    return dp[x][y]
+
+for i in range(n):
+    for j in range(n):
+        dp[i][j] = find_max(i, j)
 
 ans = 0
 for line in dp:
