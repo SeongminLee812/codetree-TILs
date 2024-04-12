fb3fd8f3e00c2bd61101bccf716a4d3604a9e7d6
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-12 12:23:45 +0900

    Title: 배낭 채우기, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240412/배낭 채우기/knapsack.py
+++ 240412/배낭 채우기/knapsack.py
@@ -1,22 +1,19 @@
 n, m = map(int, input().split())
 
-diamonds = [
-    tuple(map(int, input().split()))
-    for _ in range(n)
-]
-
-diamonds = [(0, 0)] + diamonds
-
-dp = [
-    [-1] * (m + 1)
-    for _ in range(n + 1)
-]
-dp[0][0] = 0
+weight = [0] * (n + 1)
+value = [0] * (n + 1)
 
 for i in range(1, n + 1):
-    for j in range(m + 1):
-        dp[i][j] = dp[i - 1][j]
-        if j >= diamonds[i][0] and dp[i - 1][j - diamonds[i][0]] != -1:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j - diamonds[i][0]] + diamonds[i][1])
+    weight[i], value[i] = map(int, input().split())
 
-print(max(dp[n]))
+dp = [-1] * (m + 1)
+dp[0] = 0
+
+for i in range(n):
+    for j in range(m, 0, -1):
+        if weight[i] <= j:
+            dp[j] = max(dp[j], dp[j - weight[i]] + value[i])
+        else:
+            dp[j] = dp[j]
+
+print(max(dp))
