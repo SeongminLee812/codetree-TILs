916580b5e9932536b72622c34ee72cc5609b69a2
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-13 14:05:52 +0900

    Title: 동일하게 분할, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240413/동일하게 분할/equal-partition.py
+++ 240413/동일하게 분할/equal-partition.py
@@ -10,21 +10,16 @@ if total_sum % 2 != 0:
 
 max_sum_of_group = total_sum // 2
 
-dp = [
-    [False] * (max_sum_of_group + 1)
-    for _ in range(n + 1)
-]
+dp = [False] * (max_sum_of_group + 1)
 
-dp[0][0] = True
+dp[0] = True
 
-for i in range(1, n + 1):
-    for j in range(max_sum_of_group + 1):
+for i in range(n):
+    for j in range(max_sum_of_group, 0, -1):
         if arr[i] <= j:
-            dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i]]
-        else:
-            dp[i][j] = dp[i - 1][j]
+            dp[j] = dp[j - arr[i]]
 
-if dp[n][max_sum_of_group]:
+if dp[max_sum_of_group]:
     print('Yes')
 else:
     print('No')
