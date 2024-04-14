595571794e485261ce3d1bb1ec60eaded678ffa0
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-14 14:09:44 +0900

    Title: 올바른 등식 만들기, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240414/올바른 등식 만들기/right-equality.py
+++ 240414/올바른 등식 만들기/right-equality.py
@@ -1,27 +1,19 @@
-import sys
-
 n, m = map(int, input().split())
 arr = [0] + list(map(int, input().split()))
-
-INT_MIN = -sys.maxsize
 
 dp = [
     [0] * 41
     for _ in range(n + 2)
 ]
 
-dp[2][20 - arr[1]] += 1
-dp[2][20 + arr[1]] += 1
+dp[0][20] = 1
 
-
-# 뿌리는 dp로 접근
-for i in range(2, n + 1):
+# 가져오는 dp로 작성
+for i in range(1, n + 1):
     for j in range(41):
-        if 0 <= j - arr[i] <= 40:
-            dp[i + 1][j - arr[i]] += dp[i][j]
-        if 0 <= j + arr[i] <= 40:
-            dp[i + 1][j + arr[i]] += dp[i][j]
+        if j - arr[i] >= 0:
+            dp[i][j] += dp[i - 1][j - arr[i]]
+        if j + arr[i] <= 40:
+            dp[i][j] += dp[i - 1][j + arr[i]]
 
-
-
-print(dp[n + 1][m + 20])
+print(dp[n][m + 20])
