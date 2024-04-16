32671865f0e82eec4048b68144b6c356449060d0
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-16 13:50:41 +0900

    Title: 게임 스튜디오에서 살아남기, Time: 142ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240416/게임 스튜디오에서 살아남기/surviving-in-a-game-studio.py
@@ -0,0 +1,29 @@
+n = int(input())
+
+MOD = 1e9 + 7
+
+dp = [
+        [
+            [0] * 4
+            for _ in range(4)
+        ] for _ in range(n + 1)
+    ]
+
+dp[1][0][0] = 1
+dp[1][0][1] = 1
+dp[1][1][0] = 1
+
+for i in range(1, n):
+    for j in range(3):
+        for k in range(3):
+            dp[i + 1][j][0] = (dp[i + 1][j][0] + dp[i][j][k]) % MOD
+            dp[i + 1][j][k + 1] = (dp[i + 1][j][k + 1] + dp[i][j][k]) % MOD
+            dp[i + 1][j + 1][0] = (dp[i + 1][j + 1][0] + dp[i][j][k]) % MOD
+
+
+ans = 0
+
+for j in range(3):
+    for k in range(3):
+        ans = (ans + dp[n][j][k]) % MOD
+print(int(ans % MOD))
