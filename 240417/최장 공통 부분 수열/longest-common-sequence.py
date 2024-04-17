65759fb186be8cd640b336bfa45275fb9f5172c3
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-17 16:50:06 +0900

    Title: 최장 공통 부분 수열, Time: 155ms, Memory: 32MB, Status: Passed - CodeTree

---
+++ 240417/최장 공통 부분 수열/longest-common-sequence.py
@@ -0,0 +1,27 @@
+import sys
+INT_MIN = -sys.maxsize
+
+a = input()
+b = input()
+
+a = " " + a
+b = " " + b
+
+dp = [
+    [INT_MIN] * len(b)
+    for _ in range(len(a))
+]
+
+for i in range(len(a)):
+    dp[i][0] = 0
+for j in range(len(b)):
+    dp[0][j] = 0
+
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len(a) - 1][len(b) - 1])
