51d5f5962f2a6f2d433b29e1f016db2fc2986e04
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-17 17:19:09 +0900

    Title: 편집거리, Time: 1575ms, Memory: 51MB, Status: Passed - CodeTree

--- 240417/편집거리/edit-distance.py
+++ 240417/편집거리/edit-distance.py
@@ -4,22 +4,26 @@ INT_MIN = -sys.maxsize
 A = " " + input()
 B = " " + input()
 
-dp = [
+memo = [
     [INT_MIN] * len(B)
     for _ in range(len(A))
 ]
 
 # initialize
 for i in range(len(A)):
-    dp[i][0] = i
+    memo[i][0] = i
 for j in range(len(B)):
-    dp[0][j] = j
+    memo[0][j] = j
 
-for i in range(1, len(A)):
-    for j in range(1, len(B)):
-        if A[i] == B[j]:
-            dp[i][j] = dp[i - 1][j - 1]
-        else:
-            dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
+# memoization
+def edit_distance(i, j):
+    if memo[i][j] >= 0:
+        return memo[i][j]
 
-print(dp[len(A) - 1][len(B) - 1])
+    if A[i] == B[j]:
+        memo[i][j] = edit_distance(i - 1, j - 1)
+    else:
+        memo[i][j] = min(edit_distance(i - 1, j - 1), edit_distance(i - 1, j), edit_distance(i, j - 1)) + 1
+    return memo[i][j]
+
+print(edit_distance(len(A) - 1, len(B) - 1))
