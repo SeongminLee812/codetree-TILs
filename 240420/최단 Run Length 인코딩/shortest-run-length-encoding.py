00aea92563fecbb270fa05f9cd99a3adbd5ce451
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-20 19:13:41 +0900

    Title: 최단 Run Length 인코딩, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240420/최단 Run Length 인코딩/shortest-run-length-encoding.py
+++ 240420/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -3,6 +3,8 @@ a = input()
 def run_length(strings):
     result = ""
     repeat = 1
+    if len(strings) == 1:
+        return 2
     for i in range(1, len(strings)):
         if strings[i - 1] != strings[i]:
             result += strings[i - 1]
@@ -19,7 +21,7 @@ def run_length(strings):
 ans = 100
 for _ in range(len(a) + 1):
     ans = min(ans, run_length(''.join(a)))
-
+    print(run_length(''.join(a)))
     a = list(a)
     temp = a[-1]
     for i in range(len(a) - 1, 0, -1):
