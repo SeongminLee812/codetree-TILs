2c71de37d229ebec4aea6a583c8c6d440f290fee
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-20 16:16:20 +0900

    Title: 최단 공통 상위수열, Time: 239ms, Memory: 31MB, Status: Passed - CodeTree

--- 240420/최단 공통 상위수열/shortest-common-supersubsequence.py
+++ 240420/최단 공통 상위수열/shortest-common-supersubsequence.py
@@ -1,24 +1,18 @@
-s = " " + input()
-t = " " + input()
+# 풀이 : a길이 + b길이 - lcs 길이
+# 이해 : a와 b의 합집합 원소의 개수는, 각 집합의 원소의 개수를 더한 후, 교집합의 원소의 개수를 뺀다.
+a = " " + input()
+b = " " + input()
 
 dp = [
-    [0] * len(t)
-    for _ in range(len(s))
+    [0] * len(b)
+    for _ in range(len(a))
 ]
 
-
-for i in range(len(s)):
-    dp[i][0] = i
-for j in range(len(t)):
-    dp[0][j] = j
-
-
-for i in range(1, len(s)):
-    for j in range(1, len(t)):
-        if s[i] == t[j]:
+for i in range(1, len(a)):
+    for j in range(1, len(b)):
+        if a[i] == b[j]:
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + 1
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-
-print(dp[len(s) - 1][len(t) - 1])
+print(len(a) - 1 + len(b) - 1 - dp[len(a) - 1][len(b) - 1])
