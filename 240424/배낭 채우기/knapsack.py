107edcda8b4b6cf773441455c50ccf817d199bbe
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-24 12:22:34 +0900

    Title: 배낭 채우기, Time: 153ms, Memory: 25MB, Status: Passed - CodeTree

--- 240424/배낭 채우기/knapsack.py
+++ 240424/배낭 채우기/knapsack.py
@@ -8,16 +8,12 @@ v = [0] * (n + 1)
 for i in range(1, n + 1):
     w[i], v[i] = map(int, input().split())
 
-dp = [
-    [INT_MIN] * (m + 1)
-    for _ in range(n + 1)
-]
+dp = [INT_MIN] * (m + 1)
+dp[0] = 0
 
-dp[0][0] = 0
-for i in range(1, n + 1):
-    for j in range(m + 1):
-        dp[i][j] = dp[i - 1][j]
-        if j >= w[i]:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j - w[i]] + v[i])
+for j in range(n + 1):
+    for i in range(m, -1, -1):
+        if w[j] <= i:
+            dp[i] = max(dp[i], dp[i - w[j]] + v[j])
 
-print(max(dp[n]))
+print(max(dp))
