418f2b139db45826e8e8ee02f4945ea33c705cf8
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-25 16:24:50 +0900

    Title: 그 요일은, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240425/그 요일은/the-day-of-the-day.py
+++ 240425/그 요일은/the-day-of-the-day.py
@@ -3,22 +3,26 @@ m1, d1, m2, d2 = map(int, input().split())
 target = input()
 
 month = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-diff = 0
-if m1 == m2:
-    diff = d2 - d1
-else:
-    diff += month[m1] - d1
-    diff += d2
-    for i in range(m1 + 1, m2):
-        diff += month[i]
+
+start_date = 0
+end_date = 0
+
+for i in range(1, m1):
+    start_date += month[i]
+start_date += d1
+for i in range(1, m2):
+    end_date += month[i]
+end_date += d2
+diff = end_date - start_date
 
 day_dict = {'Mon': 0, 'Tue': 1, 'Wed': 2, 'Thu': 3, 'Fri': 4, 'Sat': 5, 'Sun': 6}
+day_dict = dict(zip(day_dict.values(), day_dict.keys()))
 
 ans = 0
-div, mod = divmod(diff, 7)
+cur_day = 0
+for _ in range(diff + 1):
+    if day_dict[cur_day] == target:
+        ans += 1
+    cur_day = (cur_day + 1) % 7
 
-ans += div
-target = day_dict[target]
-if mod >= target:
-    ans += 1
 print(ans)
