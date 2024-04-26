b6e293b8d4ba9608759e0cdd735782d13a52ba2d
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-26 09:04:01 +0900

    Title: 선두를 지켜라 3, Time: 214ms, Memory: 40MB, Status: Passed - CodeTree

---
+++ 240426/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,35 @@
+n, m = map(int, input().split())
+
+A = [0] * 1000001
+B = [0] * 1000001
+
+pos = 1
+for _ in range(n):
+    v, t = map(int, input().split())
+    for _ in range(t):
+        A[pos] = A[pos - 1] + v
+        pos += 1
+
+pos = 1
+for _ in range(m):
+    v, t = map(int, input().split())
+    for _ in range(t):
+        B[pos] = B[pos - 1] +  v
+        pos += 1
+
+prev_status = -1
+status = -1
+
+ans = 0
+for i in range(1, pos):
+    if A[i] > B[i]:
+        status = 1
+    elif A[i] == B[i]:
+        status = 2
+    else: status = 0
+
+    if prev_status != status:
+        ans += 1
+    prev_status = status
+
+print(ans)
