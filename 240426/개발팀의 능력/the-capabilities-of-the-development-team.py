43faa27830ac1c2bb428646d1b90202c48cbf0e6
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-26 10:25:29 +0900

    Title: 개발팀의 능력, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240426/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,24 @@
+import sys
+
+arr = list(map(int, input().split()))
+
+ans = sys.maxsize
+total = sum(arr)
+
+for i in range(5 - 1):
+    for j in range(i, 5):
+        for k in range(5):
+            if k == i or k == j:
+                continue
+            team_a = arr[i] + arr[j]
+            team_b = arr[k]
+            team_c = total - team_a - team_b
+            if team_a == team_b or team_a == team_c or team_b == team_c:
+                continue
+            diff = max([team_a, team_b, team_c]) - min([team_a, team_b, team_c])
+            ans = min(diff, ans)
+
+if ans == sys.maxsize:
+    print(-1)
+else:
+    print(ans)
