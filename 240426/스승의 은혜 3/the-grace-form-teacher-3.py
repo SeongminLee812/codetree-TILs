dedb84c947888822440a5aac123ff5567e038120
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-26 10:43:20 +0900

    Title: 스승의 은혜 3, Time: 121ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240426/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 240426/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -8,13 +8,13 @@ total = [0] * n
 for i in range(n):
     p[i], s[i] = map(int, input().split())
     total[i] = p[i] + s[i]
-  
+
 ans = -sys.maxsize
 
 for i in range(n):
     total[i] = p[i] // 2 + s[i]
     temp = total[:]
-    
+
     temp.sort()
     cnt = 0
     student = 0
@@ -26,6 +26,8 @@ for i in range(n):
         cnt += 1
         student += 1
     ans = max(ans, student)
+    
+    total[i] *= 2
 
 
 print(ans)
