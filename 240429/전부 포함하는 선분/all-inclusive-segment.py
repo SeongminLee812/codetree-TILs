1482912f8de98dccc39ca294d7746612e26db516
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-04-29 21:08:16 +0900

    Title: 전부 포함하는 선분, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240429/전부 포함하는 선분/all-inclusive-segment.py
@@ -0,0 +1,21 @@
+import sys
+
+n = int(input())
+
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
+
+ans = sys.maxsize
+
+for i in range(n):
+    min_x1, max_x2 = sys.maxsize, 0
+    for j in range(n):
+        if i == j:
+            continue
+        min_x1 = min(min_x1, segments[j][0])
+        max_x2 = max(max_x2, segments[j][1])
+    ans = min(ans, max_x2 - min_x)
+
+print(ans)
