a2fa3ea9f4b93a305e4bf9d14f101efccf45c0e3
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-05-05 16:25:52 +0900

    Title: 2차원 폭발 게임, Time: 2988ms, Memory: 30MB, Status: Passed - CodeTree

--- 240505/2차원 폭발 게임/The-2D-bomb-game.py
+++ 240505/2차원 폭발 게임/The-2D-bomb-game.py
@@ -7,7 +7,7 @@ arr = [
     for _ in range(n)
 ]
 
-def search_and_boom(x, y):
+def search_condition(x, y):
     start_row = x
     row = x
     count = 1
@@ -15,22 +15,38 @@ def search_and_boom(x, y):
         count += 1
         row += 1
     final_row = row
-    if count >= m:
-        for row in range(start_row, final_row + 1):
-            arr[row][y] = 0
 
-def rotate(original_array):
+    if count >= m:
+        return start_row, final_row
+
+    return False
+
+def boom_from_to(start_row, final_row, col):
+    for row in range(start_row, final_row + 1):
+        arr[row][col] = 0
+
+def boom():
+    for i in range(n):
+        for j in range(n):
+            target = search_condition(i, j)
+            if target:
+                start_row, final_row = target
+                boom_from_to(start_row, final_row, j)
+
+def rotate():
     new_arr = [
         [0] * n
         for _ in range(n)
     ]
     for i in range(n):
         for j in range(n):
-            new_arr[i][j] = original_array[n - j - 1][i]
+            new_arr[i][j] = arr[n - j - 1][i]
 
-    return new_arr
+    for i in range(n):
+        for j in range(n):
+            arr[i][j] = new_arr[i][j]
 
-def pull_gravity(original_array):
+def pull_gravity():
     new_arr = [
         [0] * n
         for _ in range(n)
@@ -40,27 +56,40 @@ def pull_gravity(original_array):
         temp_arr = [0] * n
         temp_index = n - 1
         for row in range(n - 1, -1, -1):
-            if original_array[row][col]:
-                new_arr[temp_index][col] = original_array[row][col]
+            if arr[row][col]:
+                new_arr[temp_index][col] = arr[row][col]
                 temp_index -= 1
 
-    return new_arr
-
-for _ in range(k):
     for i in range(n):
         for j in range(n):
-            if arr[i][j]:
-                search_and_boom(i, j)
+            arr[i][j] = new_arr[i][j]
 
-    gravity_array = pull_gravity(arr)
-    rotate_array = rotate(gravity_array)
-    gravity_array = pull_gravity(rotate_array)
-    arr = copy.deepcopy(gravity_array)
 
-for i in range(n):
-    for j in range(n):
-        if arr[i][j]:
-            search_and_boom(i, j)
+def all_search():
+    visited = [
+        [False] * n
+        for _ in range(n)
+    ]
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] and not visited[i][j]:
+                if search_condition(i, j):
+                    return True
+    return False
+
+for _ in range(k):
+    while all_search():
+        boom()
+        # 여기서 터질 게 있으면 계속 터뜨리도록
+        pull_gravity()
+
+    rotate()
+    pull_gravity()
+
+while all_search():
+    boom()
+    pull_gravity()
+
 
 ans = 0
 for i in range(n):
