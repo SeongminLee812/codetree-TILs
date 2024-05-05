0b89f3cbe3f4ff44dad8a1aa2fda4d9fb64b221b
SeongminLee812 <105956513+SeongminLee812@users.noreply.github.com>
2024-05-05 12:04:53 +0900

    Title: 겹쳐지지 않는 두 직사각형, Time: 707ms, Memory: 29MB, Status: Passed - CodeTree

--- 240505/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
+++ 240505/겹쳐지지 않는 두 직사각형/non-overlapping-two-rectangles.py
@@ -1,45 +1,60 @@
 import sys
 
 n, m = map(int, input().split())
+
 arr = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
-def in_range(x, y):
-    return x >= 0 and x < n and y >= 0 and y < m
+def is_overlapped(first_x1, first_y1, first_x2, first_y2,
+                  second_x1, second_y1, second_x2, second_y2):
+    board = [
+        [0] * m
+        for _ in range(n)
+    ]
 
-# 사각형 하나 탐색하기
-def get_rectangle_sum(start_x, start_y, range_horizontal, range_vertical):
-    x, y = start_x, start_y
-    n, m = range_horizontal, range_vertical
+    for x in range(first_x1, first_x2 + 1):
+        for y in range(first_y1, first_y2 + 1):
+            board[x][y] += 1
+    for x in range(second_x1, second_x2 + 1):
+        for y in range(second_y1, second_y2 + 1):
+            board[x][y] += 1
+
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] >= 2:
+                return True
+    return False
+
+def rec_sum(x1, y1, x2, y2):
     result = 0
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            result += arr[x][y]
+    return result
 
-    for i in range(n + 1):
-        for j in range(m + 1):
-            if not in_range(x + i, y + j):
-                break
-            result += arr[x + i][y + j]
+def find_max(first_x1, first_y1, first_x2, first_y2):
+    result = -sys.maxsize
 
+    first_rectangle = rec_sum(first_x1, first_y1, first_x2, first_y2)
+    for second_x1 in range(n):
+        for second_y1 in range(m):
+            for second_x2 in range(second_x1, n):
+                for second_y2 in range(second_y1, m):
+                    if is_overlapped(first_x1, first_y1, first_x2, first_y2,
+                                     second_x1, second_y1, second_x2, second_y2):
+                        continue
+                    second_rectangle = rec_sum(second_x1, second_y1, second_x2, second_y2)
+                    if first_rectangle + second_rectangle > result:
+                        result = first_rectangle + second_rectangle
     return result
 
 ans = -sys.maxsize
-for x in range(n):
-    for y in range(m):
-        for i in range(n - x):
-            for j in range(m - y):
-                first_rectangle = get_rectangle_sum(x, y, i, j)
-
-                # 여기서부터 두번째 사각형 돌리기
-                for second_x in range(n):
-                    for second_y in range(m):
-                        # 첫번째 사각형 안에 들어오는 경우 제외
-                        if second_x <= x + i and second_y <= y + j:
-                            continue
-                        for k in range(n - second_x):
-                            for l in range(m - second_y):
-                                second_rectangle = get_rectangle_sum(second_x, second_y, k, l)
-
-                                ans = max(ans, first_rectangle + second_rectangle)
+for first_x1 in range(n):
+    for first_y1 in range(m):
+        for first_x2 in range(first_x1, n):
+            for first_y2 in range(first_y1, m):
+                ans = max(ans, find_max(first_x1, first_y1, first_x2, first_y2))
 
 print(ans)
